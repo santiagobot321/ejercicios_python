be37f66bf90c442fe713cf92b785275b3bb43be5
santiagobot321 <boteroramirezsantiago@gmail.com>
2025-04-24 08:38:45 -0500

    más ejercicios

---
+++ semana_1/ejerciciosavan.py
@@ -0,0 +1,38 @@
+# #Pedimos la cantidad de kilometros a convertir
+# cantidad = float(input("Escribe la cantidad de km a convertir: "))
+
+
+# #Definimos los cálculos para las diferentes conversiones
+# metros = cantidad*1000
+# centimetros = cantidad*100000
+# milimetros = cantidad*1000000
+
+# print(f'{cantidad} KM = {metros:.2f} metros | {centimetros:.2f} centímetros | {milimetros:.2f} milímetros')
+
+
+
+
+
+#__________________________________________________________________________________________________________________________________________________________
+
+
+# seg = float(input('Ingresa la cantidad de segundos: '))
+
+# horas = seg//3600
+# min = seg%3600
+# minutos = min//60
+# secu = seg%60
+# segundos = min//60
+
+# print(f'\n{seg} segundos son: {horas} horas {minutos} minutos {secu} segundos')
+
+
+
+
+
+#____________________________________________________________________________________________________________________________________________________________
+
+
+
+
+
